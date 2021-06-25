258d19854490b32f1368a199b007c449cb2958f2
Chris Sampson <65880029+JorganBonstragel@users.noreply.github.com>
2021-06-25 15:02:05 -0400

    adds projects object and project parsing

--- js/app.js
+++ js/app.js
@@ -1,10 +1,36 @@
+// Elements 
 const mainSlider = $('#main-slick')
+const ProSlider = $('#pro-slick')
+const ProtoSlider = $('#proto-slick')
+const proProjects =  projects.ProProjects
+const protoProjects = projects.ProtoProjects
 
-
-
+// Creates Proejct cards and inserts into DOM
+BuildProjects = () => {
+    // Pro
+    proProjects.forEach(p => {
+        let card = `
+            <div class="card">
+                ${p.title}
+                ${p.client}
+            </div>
+        `
+        document.querySelector('#pro-slick').insertAdjacentHTML('beforeend', card)
+    });
+    // Prototypes
+    protoProjects.forEach(p => {
+        let card = `
+            <div class="card">
+                ${p.title}
+                ${p.tags}
+            </div>
+        `
+        document.querySelector('#proto-slick').insertAdjacentHTML('beforeend', card)
+    });
+}
 
 //------ On Load Event ---------
-window.onload =() => {
+window.onload = () => {
     // Init Main Vertical Slider
     mainSlider.slick({
         vertical: true,
@@ -12,7 +38,19 @@ window.onload =() => {
         arrows:false,
         verticalSwiping:true,
     })
+    // Append Projects to the DOM
+    BuildProjects()
+    // Init Project Sliders
+    ProSlider.slick({
+        infinite:true,
+        arrows:true
+    })
+    ProtoSlider.slick({
+        infinite:true
+    })
+    
 }
+
 
 // Hook into slick with on scroll event
 mainSlider.on('wheel', (function(e){
---
+++ js/projects.js
@@ -0,0 +1,67 @@
+const projects = {
+    "ProProjects":[
+        {
+          "title":"C.E.M WebSite",
+          "client":"Chautauqua Energy Managment",
+          "link": "",
+          "img":"",
+          "desc": "",
+          "tags":["Express","EJS","BootStrap5"]  
+        },
+        {
+            "title":"PingPlotter MultiViewer",
+            "client":"Digitell Inc.",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags":["Express","MongoDB","Skeleton"]
+        },
+        {
+            "title":"Storage CO Site",
+            "client":"Esteem Managment",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["WIX"]
+        },
+        {
+            "title":"Art Portfolio",
+            "client":"Jody Vankeuren",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["Baguette Box", "Responsive"]
+        },
+        {
+            "title":"Folder Duplicator",
+            "client":"Digitell Inc.",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["Powershell V5.1", "ACL Objects", "PowerShell Forms"]
+        }
+    ],
+    "ProtoProjects":[
+        {
+            "title":"Yelp Camp",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["Express", "MongoDB", "Passport"]
+        },
+        {
+            "title":"Rock Paper Scissors",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["Express", "MongoDB", "Passport"]
+        },
+        {
+            "title":"Calculator",
+            "link": "",
+            "img": "",
+            "desc": "",
+            "tags": ["Express", "MongoDB", "Passport"]
+        }
+    ]
+}
